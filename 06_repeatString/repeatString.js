56e87d17971afc5b31da6d90ed4a1da9604b563e
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-07-14 09:17:45 +0800

    feat: Used for loop to solve repeating string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    const originalStr = str;
+    if(num < 0){
+        return "Number should be greater than zero.";
+    }
+    for(let i = 1; i < num; i++){
+        str += originalStr;
+    }
+    return str;
 };
 
 // Do not edit below this line
