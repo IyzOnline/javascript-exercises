bb63b97346a56f77048c1c03e290a6ca15c18fe6
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-07-14 09:33:32 +0800

    feat: Use for loop to sum numbers between a and b

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if(firstNum <= 0 || secondNum <= 0){
+        return "ERROR";
+    } else if (firstNum === secondNum){
+        return firstNum + secondNum;
+    }
 
+    const sum = 0;
+    const [greaterNum, lesserNum] = firstNum > secondNum ? [firstNum, secondNum] : [secondNum, firstNum];
+    
+    for (let i = lesserNum; i <= greaterNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
