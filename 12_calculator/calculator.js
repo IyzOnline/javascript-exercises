aebb329f201e3b4868ad6ef60e574337ad4cf3ea
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-07 09:02:26 +0800

    feat: Solved add, subtract, and sum functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,13 +1,18 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let initialVal = 0;
+
+  return arr.reduce(
+    (accumulator, currentValue) => accumulator += currentValue,
+    initialVal
+  );
 };
 
 const multiply = function() {
