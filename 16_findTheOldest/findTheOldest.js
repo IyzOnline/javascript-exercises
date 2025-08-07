7f201ea4a0478f2255a29cd08a22f8ef9c28dcd3
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-07 10:35:31 +0800

    feat: Subtract year of death or current year from year of birth to solve oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldestPerson = arr.reduce((oldestPerson, currentPerson) => {
+        const oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        
+        const currentPersonAge = currentPerson.yearOfDeath ? currentPerson.yearOfDeath - currentPerson.yearOfBirth : new Date().getFullYear() - currentPerson.yearOfBirth;
+        if (currentPersonAge > oldestPersonAge){
+            oldestPerson = currentPerson;
+        }
+        return oldestPerson;
+    });
 
+    return oldestPerson;
 };
 
 // Do not edit below this line
