0a9996a231c8f720386d909d9c1d1046b0a32e5b
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-07 09:47:10 +0800

    feat: Iterate over each character pair to determine if tstring is palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
 const palindromes = function (text) {
     let inputText = text.toLowerCase();
     inputText.replaceAll(/[^a-zA-Z0-9]/g, "");
+
+
+    for(let i = 0; i < inputText.length/2; i--){
+        if(inputText[i] !== inputText[inputText.length - 1 - i]){
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
