8e48abcd42ce6c612b62364dcc5b0dfa01323d4a
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-07 09:45:07 +0800

    feat: Convert entire text to lowercase and use regex to remove non-alphabetic and non-numeric characters

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let inputText = text.toLowerCase();
+    inputText.replaceAll(/[^a-zA-Z0-9]/g, "");
 };
 
 // Do not edit below this line
