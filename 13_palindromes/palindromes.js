9c139163fb784706b773d51b2aaa169b129b5f1f
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-07 09:50:00 +0800

    fix: Looping variable should increment instead of decrement

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -2,7 +2,7 @@ const palindromes = function (text) {
     let inputText = text.toLowerCase().replaceAll(/[^a-zA-Z0-9]/g, "");
 
 
-    for(let i = 0; i < inputText.length/2; i--){
+    for(let i = 0; i < inputText.length/2; i++){
         if(inputText[i] !== inputText[inputText.length - 1 - i]){
             return false;
         }
