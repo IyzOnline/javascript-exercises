0ef2fced9603e994ae89942b06c145100af7d8dc
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-07 09:49:33 +0800

    fix: Take the returned string of replaceAll

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,5 @@
 const palindromes = function (text) {
-    let inputText = text.toLowerCase();
-    inputText.replaceAll(/[^a-zA-Z0-9]/g, "");
+    let inputText = text.toLowerCase().replaceAll(/[^a-zA-Z0-9]/g, "");
 
 
     for(let i = 0; i < inputText.length/2; i--){
