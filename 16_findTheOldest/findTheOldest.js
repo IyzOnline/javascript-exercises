760cc0f673a51d4a49b948ec08010ea23d9fc350
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-08-07 10:42:43 +0800

    fix: Insert missing ternary conditional to fix NaN oldestPersonAge error

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,8 +1,9 @@
 const findTheOldest = function(arr) {
     const oldestPerson = arr.reduce((oldestPerson, currentPerson) => {
-        const oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        const oldestPersonAge = oldestPerson.yearOfDeath ? oldestPerson.yearOfDeath - oldestPerson.yearOfBirth: new Date().getFullYear() - oldestPerson.yearOfBirth;
         
         const currentPersonAge = currentPerson.yearOfDeath ? currentPerson.yearOfDeath - currentPerson.yearOfBirth : new Date().getFullYear() - currentPerson.yearOfBirth;
+        
         if (currentPersonAge > oldestPersonAge){
             oldestPerson = currentPerson;
         }
